3c8b4cfac465f328501cb1443597f780810669dd
Tim Xu <ting.x@wustl.edu>
2023-11-09 20:41:11 -0600

    Added Style for JobList JobDetail

--- my-app/src/JobDetail.js
+++ my-app/src/JobDetail.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
-
+import './JobDetail.css'; // Ensure you have the CSS file for styling
 function JobDetail() {
   const { job_fccid } = useParams(); // Corrected to job_fccid
   const [job, setJob] = useState(null);
@@ -70,9 +70,15 @@ function JobDetail() {
   }
 
   return (
-    <div>
+    <div className="job-detail">
       <h2>{job.job_title}</h2>
-      {/* Display all job details */}
+      <p><strong>Company:</strong> {job.company_name}</p>
+      <p><strong>Link:</strong> <a href={job.job_link} target="_blank" rel="noopener noreferrer">{job.job_link}</a></p>
+      <p><strong>Location:</strong> {job.job_location}</p>
+      <p><strong>Post Date:</strong> {new Date(job.post_date).toLocaleDateString()}</p>
+      {job.salary && <p><strong>Salary:</strong> {job.salary.toLocaleString()}</p>}
+      <p><strong>Type:</strong> {job.job_type}</p>
+      <p><strong>Description:</strong> {job.job_description}</p>  
       <button onClick={handleApplyClick}>Apply</button>
       <button onClick={handleMarkAsApplied}>{applicationStatus}</button>
     </div>
--- my-app/src/JobList.js
+++ my-app/src/JobList.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import './JobList.css'; // Ensure you have the CSS file for styling
 
 function JobList() {
   const [jobs, setJobs] = useState([]);
   const [error, setError] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
+  const [jobsPerPage] = useState(10);
 
   useEffect(() => {
     const fetchJobs = async () => {
@@ -23,32 +26,45 @@ function JobList() {
     fetchJobs();
   }, []);
 
+  // Get current jobs for pagination
+  const indexOfLastJob = currentPage * jobsPerPage;
+  const indexOfFirstJob = indexOfLastJob - jobsPerPage;
+  const currentJobs = jobs.slice(indexOfFirstJob, indexOfLastJob);
+
+  // Change page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
   return (
     <div>
       <h2>Job Listings</h2>
       {error && <p className="error">{error}</p>}
-      {jobs.length ? (
-        <ul>
-          {jobs.map((job) => (
-            // Wrap the job title in a Link component
-            <li key={job.job_fccid}>
+      {currentJobs.length ? (
+        <ul className="job-list">
+          {currentJobs.map((job) => (
+            <li key={job.job_fccid} className="job-item">
               <Link to={`/jobs/${job.job_fccid}`}>
                 <h3>{job.job_title}</h3>
               </Link>
               <p><strong>Company:</strong> {job.company_name}</p>
               <p><strong>Location:</strong> {job.job_location}</p>
               <p><strong>Post Date:</strong> {new Date(job.post_date).toLocaleDateString()}</p>
-              {job.salary && <p><strong>Salary:</strong> {job.salary}</p>}
+              {job.salary && <p><strong>Salary:</strong> {job.salary.toLocaleString()}</p>}
               <p><strong>Type:</strong> {job.job_type}</p>
               <p><strong>Description:</strong> {job.job_description}</p>
               <a href={job.job_link} target="_blank" rel="noopener noreferrer">Job Link</a>
-              {/* Add more fields as necessary */}
             </li>
           ))}
         </ul>
       ) : (
         <p>No job listings available.</p>
       )}
+      <div className="pagination">
+        {Array.from({ length: Math.ceil(jobs.length / jobsPerPage) }, (_, index) => (
+          <button key={index} onClick={() => paginate(index + 1)} className="page-item">
+            {index + 1}
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
